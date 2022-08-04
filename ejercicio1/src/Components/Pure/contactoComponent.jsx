740c8a22a5afc8e81c59d2937f2501bbee893c2c
Carlos Díaz <cdperez.85@gmail.com>
2022-08-04 13:46:13 +0200

    actualizado ejercicio 1

--- ejercicio1/src/Components/Pure/contactoComponent.jsx
+++ ejercicio1/src/Components/Pure/contactoComponent.jsx
@@ -7,7 +7,8 @@ import { Contacto } from '../Models/Contacto';
 const ContactoComponent = ({contacto}) => {
     return (
         <div>
-            <h1></h1>
+            <h1>{contacto.nombre} {contacto.apellidos}</h1>
+            <h2>{contacto.email}</h2>
         </div>
     );
 };
